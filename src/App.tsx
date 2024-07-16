60b5634571bd45ef59ffc2f9766ef4b2c30d8482
Gasatrya <gasatrya.dev@gmail.com>
2024-07-17 01:21:35 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Hello from "./components/Hello";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <Hello />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -33,5 +31,4 @@ function App() {
     </>
   );
 }
-
 export default App;
